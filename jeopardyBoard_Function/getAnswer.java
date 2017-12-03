796f1b9efead8e06517f757e81c01d28750539f9
elin3d1 <elindse3@aum.edu>
2017-12-03 12:01:21 -0600

    Add files via upload
    Class used to retrieve an answer from the Database.

---
+++ jeopardyBoard_Function/getAnswer.java
@@ -0,0 +1,69 @@
+import java.util.List;
+public class getAnswer{
+	
+	/**
+	 * Get Answer from database with sectionID
+	 * @param section
+	 * @param difficultyInt
+	 * @return
+	 */
+	public static String fromList (int section, int difficultyInt){
+		
+		scDataBase dB = new scDataBase();
+		List<scRecord> records = dB.getRecords();
+		
+		for(int i=0;i<records.size();i++)
+		{
+			if((records.get(i).getDifficulty() == difficultyInt) && (records.get(i).getSection() == section))
+				return records.get(i).getAnswerHtml();
+		}
+		return "No Answer Found.";
+	}
+	
+	/**
+	 * Get Answer from database with subject name
+	 * @param subject
+	 * @param difficultyInt
+	 * @return
+	 */
+	public static String fromList (String subject, int difficultyInt){
+		
+		scDataBase dB = new scDataBase();
+		List<scRecord> records = dB.getRecords();
+		
+		for(int i=0;i<records.size();i++)
+		{
+			if((records.get(i).getDifficulty() == difficultyInt) && (records.get(i).getSubject() == subject))
+				return records.get(i).getAnswerHtml();
+		}
+		return "No Answer Found.";
+	}
+	
+	/**
+	 * Get Answer from database with sectionID and subsectionID
+	 * @param section
+	 * @param subsection
+	 * @param difficultyInt
+	 * @return
+	 */
+	public static String fromList (int section, int subsection, int difficultyInt){
+
+		scDataBase dB = new scDataBase();
+		List<scRecord> records = dB.getRecords();
+		
+		for(int i=0;i<records.size();i++)
+		{
+			if((records.get(i).getDifficulty() == difficultyInt))
+			{
+				if(records.get(i).getSection() == section)
+				{
+					if(records.get(i).getSubsection() == subsection)
+					{
+						return records.get(i).getAnswerHtml();
+					}
+				}
+			}
+		}
+		return "No Answer Found.";
+	}
+}
